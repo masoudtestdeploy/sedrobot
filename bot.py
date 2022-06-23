3c829c169a73a8d8cc10da5605018230473ef927
masoudtestdeploy <95133886+masoudtestdeploy@users.noreply.github.com>
2022-06-24 01:16:51 +0430

    Update bot.py

--- bot.py
+++ bot.py
@@ -197,74 +197,6 @@ def Add_TR(Magnet):
     
 
     return res
-async def progress_for_pyrogram(
-    current,
-    total,
-    ud_type,
-    message,
-    start
-):
-    now = time.time()
-    diff = now - start
-    if round(diff % 10.00) == 0 or current == total:
-        # if round(current / total * 100, 0) % 5 == 0:
-        percentage = current * 100 / total
-        speed = current / diff
-        elapsed_time = round(diff) * 1000
-        time_to_completion = round((total - current) / speed) * 1000
-        estimated_total_time = elapsed_time + time_to_completion
-
-        elapsed_time = TimeFormatter(milliseconds=elapsed_time)
-        estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)
-
-        progress = "[{0}{1}] \nP: {2}%\n".format(
-            ''.join(["█" for i in range(math.floor(percentage / 5))]),
-            ''.join(["░" for i in range(20 - math.floor(percentage / 5))]),
-            round(percentage, 2))
-
-        tmp = progress + "{0} of {1}\nSpeed: {2}/s\nETA: {3}\n".format(
-            humanbytes(current),
-            humanbytes(total),
-            humanbytes(speed),
-            # elapsed_time if elapsed_time != '' else "0 s",
-            estimated_total_time if estimated_total_time != '' else "0 s"
-        )
-        try:
-            await message.edit(
-                text="{}\n {}".format(
-                    ud_type,
-                    tmp
-                )
-            )
-        except:
-            pass
-
-
-def humanbytes(size):
-    # https://stackoverflow.com/a/49361727/4723940
-    # 2**10 = 1024
-    if not size:
-        return ""
-    power = 2**10
-    n = 0
-    Dic_powerN = {0: ' ', 1: 'Ki', 2: 'Mi', 3: 'Gi', 4: 'Ti'}
-    while size > power:
-        size /= power
-        n += 1
-    return str(round(size, 2)) + " " + Dic_powerN[n] + 'B'
-
-
-def TimeFormatter(milliseconds: int) -> str:
-    seconds, milliseconds = divmod(int(milliseconds), 1000)
-    minutes, seconds = divmod(seconds, 60)
-    hours, minutes = divmod(minutes, 60)
-    days, hours = divmod(hours, 24)
-    tmp = ((str(days) + "d, ") if days else "") + \
-        ((str(hours) + "h, ") if hours else "") + \
-        ((str(minutes) + "m, ") if minutes else "") + \
-        ((str(seconds) + "s, ") if seconds else "") + \
-        ((str(milliseconds) + "ms, ") if milliseconds else "")
-    return tmp[:-2]
 
 @bot.on_message(filters.command("start"))
 async def start_command(bot, message):
@@ -313,18 +245,16 @@ async def del_file_info(bot, message):
     name = seedr.get_file(p_id)["name"]
 
     try:
-        await bot.message.edit('در حال دانلود ...')
-        with open(name, 'wb') as f:
-            response = requests.get(link, stream=True)
-            f.write(response.content)
-        f.close
-        await message.message.reply_video(name, caption='دانلود شده توسط : @kenzomovie')
-        os.remove("KN." + name + '.mp4')
+        #await bot.message.edit('در حال دانلود ...')
+        response = requests.get(link, stream=True)
+        open(name, 'wb').write(response.content)
+        await bot.message.reply_video(name, caption='دانلود شده توسط : @kenzomovie')
+        os.remove(name)
 
-        print("ok sen shod")
+        print("ok send shod")
     except:
         print("nemisheeeeeee")
-        await message.reply_text(link+"    ///   "+name)
+        await message.reply_text(link+"    \n\n   "+name)
 
     
     #await message.reply_text(link+"    ///   "+name)
