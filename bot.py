5f33273683c3d0eed19593ef5ae945a7e9d64f48
masoudtestdeploy <95133886+masoudtestdeploy@users.noreply.github.com>
2022-06-23 20:31:12 +0430

    Update bot.py

--- bot.py
+++ bot.py
@@ -23,6 +23,8 @@ from urllib.parse import quote_plus
 from seedr import SeedrAPI
 
 import requests
+import time
+import math 
 
 api_id = 2669389
 api_hash = "59f112100d19186dc03cd93fb7f2904a"
@@ -194,6 +196,74 @@ def Add_TR(Magnet):
     
 
     return res
+async def progress_for_pyrogram(
+    current,
+    total,
+    ud_type,
+    message,
+    start
+):
+    now = time.time()
+    diff = now - start
+    if round(diff % 10.00) == 0 or current == total:
+        # if round(current / total * 100, 0) % 5 == 0:
+        percentage = current * 100 / total
+        speed = current / diff
+        elapsed_time = round(diff) * 1000
+        time_to_completion = round((total - current) / speed) * 1000
+        estimated_total_time = elapsed_time + time_to_completion
+
+        elapsed_time = TimeFormatter(milliseconds=elapsed_time)
+        estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)
+
+        progress = "[{0}{1}] \nP: {2}%\n".format(
+            ''.join(["█" for i in range(math.floor(percentage / 5))]),
+            ''.join(["░" for i in range(20 - math.floor(percentage / 5))]),
+            round(percentage, 2))
+
+        tmp = progress + "{0} of {1}\nSpeed: {2}/s\nETA: {3}\n".format(
+            humanbytes(current),
+            humanbytes(total),
+            humanbytes(speed),
+            # elapsed_time if elapsed_time != '' else "0 s",
+            estimated_total_time if estimated_total_time != '' else "0 s"
+        )
+        try:
+            await message.edit(
+                text="{}\n {}".format(
+                    ud_type,
+                    tmp
+                )
+            )
+        except:
+            pass
+
+
+def humanbytes(size):
+    # https://stackoverflow.com/a/49361727/4723940
+    # 2**10 = 1024
+    if not size:
+        return ""
+    power = 2**10
+    n = 0
+    Dic_powerN = {0: ' ', 1: 'Ki', 2: 'Mi', 3: 'Gi', 4: 'Ti'}
+    while size > power:
+        size /= power
+        n += 1
+    return str(round(size, 2)) + " " + Dic_powerN[n] + 'B'
+
+
+def TimeFormatter(milliseconds: int) -> str:
+    seconds, milliseconds = divmod(int(milliseconds), 1000)
+    minutes, seconds = divmod(seconds, 60)
+    hours, minutes = divmod(minutes, 60)
+    days, hours = divmod(hours, 24)
+    tmp = ((str(days) + "d, ") if days else "") + \
+        ((str(hours) + "h, ") if hours else "") + \
+        ((str(minutes) + "m, ") if minutes else "") + \
+        ((str(seconds) + "s, ") if seconds else "") + \
+        ((str(milliseconds) + "ms, ") if milliseconds else "")
+    return tmp[:-2]
 
 @bot.on_message(filters.command("start"))
 async def start_command(bot, message):
@@ -239,6 +309,20 @@ async def del_file_info(bot, message):
     #response = requests.get(link)
     name = seedr.get_file(p_id)["name"]
    # open("test.mp4","wb").write(response.content)
+    await bot.send_video(
+                    chat_id=message.message.chat.id,
+                    video="https://rs17.seedr.cc/ff_get/1188588415/Obi-Wan.Kenobi.S01E06.720p.10bit.WEBRip.2CH.x265.HEVC-PSA.mkv?st=yyzXaakonEhHc0A5y-krrg&e=1656086433",
+                    caption="caption",
+                    parse_mode="HTML",
+                    #duration=duration,
+                    #width=width,
+                    #height=height,
+                    thumb="https://cdn.pixabay.com/photo/2013/08/29/02/47/eiffel-tower-176935_960_720.jpg",
+                    supports_streaming=True,
+                    #reply_to_message_id=update.message.reply_to_message.message_id,
+                    progress=progress_for_pyrogram,
+                     )
+
     #await bot.send_video(message.chat.id, "test.mp4", caption="video caption")
     await message.reply_text(link+"///"+name)
 
